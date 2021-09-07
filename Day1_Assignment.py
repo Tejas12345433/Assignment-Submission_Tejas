044a73003a3e127a5d76457a389ef41cb63893b0
Tejas12345433 <90244432+Tejas12345433@users.noreply.github.com>
2021-09-07 22:19:35 +0530

    Day1_Assignment Submitted

---
+++ Day1_Assignment.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+a=200
+
+
+# In[12]:
+
+
+b=100
+
+
+# In[18]:
+
+
+#Subraction
+a-b
+
+
+# In[14]:
+
+
+#Addition
+a+b
+
+
+# In[15]:
+
+
+#Multipication
+a*b
+
+
+# In[16]:
+
+
+#Division
+a/b
+
+
+# In[19]:
+
+
+#Modulus
+a%b
+
+
+# In[ ]:
+
+
+
+
