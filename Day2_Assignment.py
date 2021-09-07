42d4d423ea9a36500ce64bb51d813d96b6751bfa
Tejas12345433 <90244432+Tejas12345433@users.noreply.github.com>
2021-09-07 22:22:20 +0530

    Day2 Assignment Submitted

---
+++ Day2_Assignment.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[14]:
+
+
+lst1=[1,2,3,4,5]
+lst2=[2,3,4,5,6]
+lst3=[4,5,6,7,8]
+
+
+# In[15]:
+
+
+arr1=np.array([lst1,lst2,lst3])
+
+
+# In[16]:
+
+
+arr1
+
+
+# In[17]:
+
+
+arr1[1:3,1:3]
+
+
+# In[18]:
+
+
+arr1[1:2,1:4]
+
+
+# In[ ]:
+
+
+
+
